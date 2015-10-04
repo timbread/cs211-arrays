bdbfb31851abaf308899e3bec20bc7f282fd4fbc
timbread <tim.bread@yandex.ru>
2015-10-04 18:16:26 +0300

    Изменен main.cpp
    Добавлены вызовы функций task_1, task_2, task_4
    Добавлен тест функции task_3_zero
    Добавлена демонстрация task_5_copy, task_6_poor_copy, печать task_7_print

--- main.cpp
+++ main.cpp
@@ -3,5 +3,23 @@
 #include <cassert>
 
 int main() {
-	
+
+	task_1();
+	task_2();
+	assert(task_3_zero() == 0);
+	task_4();
+
+	int arr1[5]{ 1, 2, 3, 4, 5 };
+	int arr2[5]{ 0 };
+	task_5_copy(arr1, arr2, 5);
+
+	task_7_print(arr2, 5);
+
+	std::cout << std::endl;
+
+	int arr3[5]{ -1, -2, -3, -4, -5 };
+	int arr4[5]{ 0 };
+	task_6_poor_copy(arr3, arr4, 5);
+
+	task_7_print(arr4, 5);
 }
